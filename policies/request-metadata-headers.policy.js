fbbd421c7ed9436e6e525d94fbc684c77762daf9
Tom Conchie <tconchie@greenchef.com>
2020-06-24 12:56:59 -0600

    [GROW-144] Adds null check for useragent and ip to avoid errors

--- policies/request-metadata-headers.policy.js
+++ policies/request-metadata-headers.policy.js
@@ -5,12 +5,10 @@ module.exports = {
   },
   policy: () => {
     return (req, res, next) => {
-      const ip = req.headers['x-origin-client-ip'] || req.connection.remoteAddress.replace(/[:f]/g, '');
+      const ip = req.headers['x-origin-client-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress.replace(/[:f]/g, '');
       const userAgent = req.headers['x-origin-client-user-agent'] || req.get('user-agent');
-      console.log(ip),
-      console.log(userAgent)
-      req.headers['x-origin-client-ip'] = ip;
-      req.headers['x-origin-client-user-agent'] = userAgent;
+      if (ip) req.headers['x-origin-client-ip'] = ip;
+      if (userAgent) req.headers['x-origin-client-user-agent'] = userAgent;
       next();
     };
   },
